5d28d9eb56fb9da6b431d6b22fd2aca47a577b70
Kate <sneakyk8@gmail.com>
2015-04-04 10:27:46 -0700

    moved .downcase and .strip with gets method, made notes

--- animals.rb
+++ animals.rb
@@ -1,13 +1,16 @@
 puts "Enter an animal that makes a sound."
 
-animal = gets.chomp
+animal = gets.chomp.downcase.strip
 
 puts "What sound does it make?"
 
-sound = gets.chomp
+sound = gets.chomp.downcase.strip
 
 puts "How many times should the animal make the noise?"
 
 times = gets.chomp.to_i
 
-puts "The " + animal.downcase.strip + " goes " + (sound.downcase.strip + " ")*times 
+puts "The " + animal + " goes " + (sound + " ")*times 
+
+# it's preferable to 'sanitize' the variable when it's given to you (during gets)
+#gsub method to remove middle spaces?
